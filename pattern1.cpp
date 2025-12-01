d43fa8d427d10aff99e63383b2ae9f124fca6aeb
doddiyasaswini <doddiyasaswini686@gmail.com>
2025-12-01 14:26:35 +0530

    Implement right half pyramid star pattern in C++
    The code prints a right half pyramid pattern of stars based on user input.

---
+++ pattern1.cpp
@@ -0,0 +1,27 @@
+/*Given an integer N, print N rows of right half pyramid pattern. 
+In right half pattern of N rows, the first row has 1 star, second row has 2 stars and so on till the Nth row which has N stars. All the stars are left aligned.
+
+Input: 3
+Output: 
+*
+**
+***
+
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+cout<<"Enter n value:";
+cin>>n;
+for(int i=0;i<n;i++)
+{
+  for(int j=0;j<=n;j++)
+{
+  cout<<"* ";
+}
+cout<<endl;
+}
+return 0;
+}
