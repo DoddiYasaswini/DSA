c90fc5f20c8e6938e3b9158396bc47f9a92b649b
doddiyasaswini <doddiyasaswini686@gmail.com>
2025-12-01 14:42:28 +0530

    Implement inverted right half pyramid pattern printing.
    Implement inverted right half pyramid pattern printing.

---
+++ Pattern/pattern2.cpp
@@ -0,0 +1,29 @@
+/*
+Given an integer N, print N rows of inverted right half pyramid pattern. 
+In inverted right half pattern of N rows, the first row has N number of stars, 
+second row has (N - 1) number of stars and so on till the Nth row which has only 1 star.
+
+Input: n = 5
+Output:
+*****
+****
+***
+**
+*
+
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+cin>>n;
+for(int i=n;i>=0;i--)
+{
+  for(int j=0;j<=i;j++){
+cout<<"* ";
+}
+cout<<endl;
+}
+return 0;
+}
